c907c8e66210d91cab12278b55850ba174275552
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-02-24 17:40:49 +0000

    Adding new CSS via SASS.

--- response-stack.php
+++ response-stack.php
@@ -11,6 +11,7 @@ License: GPL2
 */
 
 /*  Developed for the CFO Magazine
+	Styling and format based on work with Human Made.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
